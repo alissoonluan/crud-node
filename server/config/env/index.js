fc11226b3def548c496b8949b7e37b81b0ed7426
alissoonluan <alisson_luan_@hotmail.com>
2020-02-04 13:35:56 -0300

    ajustes

---
+++ server/config/env/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: 'development',
+    nomeApi: 'api-node',
+    urlApi: 'http://localhost',
+    portApi: 3333,
+    dataConfig: {
+      MYSQL: {
+        database: 'node',
+        host: 'localhost',
+        port: '8081',
+        user: 'root',
+        password: '1234'
+      }
+    }
+  };
+  
